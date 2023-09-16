f1a4d8f56bb3bc7c952bedec6fa394d432331c2d
Fernando Cunha <cunhafernandofj@gmail.com>
2023-09-16 18:38:47 -0300

    Created the comment views

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,7 @@ class CommentsController < ApplicationController
 
     def create
         @post = Post.find(params[:post_id])
-        @comment = @post.comments.create(comments_params)
+        @comment = @post.comments.create(comment_params)
         @comment.user = current_user
         if @comment.save
             flash[:notice] = "Comment has been created"
@@ -21,13 +21,25 @@ class CommentsController < ApplicationController
         redirect_to post_path(@post)
     end
 
+    def update
+        @comment = @post.comments.find(params[:id])
+
+        respond_to do |format|
+            if @comment.update(comment_params)
+                format.html { redirect_to post_url(@post), notice: 'Comment has been updated' }
+            else
+                format.html { redirect_to post_url(@post), alert: 'Comment was not updated!' }
+            end
+        end
+    end
+
     private
 
     def set_post
         @post = Post.find(params[:post_id])
     end
 
-    def comments_params
+    def comment_params
         params.require(:comment).permit(:body)
     end
 end
